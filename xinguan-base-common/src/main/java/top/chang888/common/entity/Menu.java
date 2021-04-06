95f168a978b7d46548be600d546ad6114088f185
Changyw <cyw980406@163.com>
2021-04-06 17:19:23 +0800

    添加菜单管理的前端页面

---
+++ xinguan-base-common/src/main/java/top/chang888/common/entity/Menu.java
@@ -0,0 +1,68 @@
+package top.chang888.common.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+
+import java.util.Date;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * <p>
+ * 菜单表
+ * </p>
+ *
+ * @author changyw
+ * @since 2021-04-06
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@TableName("tb_menu")
+@ApiModel(value="Menu对象", description="菜单表")
+public class Menu implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "菜单/按钮ID")
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    @ApiModelProperty(value = "上级菜单ID")
+    private Long parentId;
+
+    @ApiModelProperty(value = "菜单/按钮名称")
+    private String menuName;
+
+    @ApiModelProperty(value = "菜单URL")
+    private String url;
+
+    @ApiModelProperty(value = "权限标识")
+    private String perms;
+
+    @ApiModelProperty(value = "图标")
+    private String icon;
+
+    @ApiModelProperty(value = "类型 0菜单 1按钮")
+    private String type;
+
+    @ApiModelProperty(value = "排序")
+    private Long orderNum;
+
+    @ApiModelProperty(value = "创建时间")
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    @ApiModelProperty(value = "修改时间")
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date modifiedTime;
+
+    @TableLogic(value = "1", delval = "0")
+    @ApiModelProperty(value = "0：不可用，1：可用")
+    private Integer available;
+
+    @ApiModelProperty(value = "0:不展开，1：展开")
+    private Integer open;
+
+}
