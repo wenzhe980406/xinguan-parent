46a5c5e2cbf469f1534e9a7943ca62a60d809ac4
Changyw <cyw980406@163.com>
2021-04-08 17:29:17 +0800

    角色权限管理后台代码

--- xinguan-base-web/src/main/java/top/chang888/system/controller/RoleController.java
+++ xinguan-base-web/src/main/java/top/chang888/system/controller/RoleController.java
@@ -1,9 +1,20 @@
 package top.chang888.system.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import top.chang888.common.entity.Role;
+import top.chang888.common.response.Result;
+import top.chang888.system.service.RoleService;
+
+import java.util.List;
+import java.util.Objects;
 
 /**
  * <p>
@@ -17,5 +28,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/system/role")
 public class RoleController {
 
+    @Autowired
+    private RoleService roleService;
+
+    public Result get(@RequestParam(defaultValue = "1") Integer currentPage,
+                      @RequestParam(defaultValue = "10") Integer size,
+                      @Nullable @RequestParam(defaultValue = "") String name) {
+        try {
+            QueryWrapper<Role> wrapper = new QueryWrapper<>();
+            if (!ObjectUtils.isEmpty(name)){
+                wrapper.eq("role_name", name);
+            }
+
+            List<Role> roles = roleService.get(currentPage, size, wrapper);
+        }
+    }
 }
 
